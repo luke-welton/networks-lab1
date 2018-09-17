41e6f0f6b3fbb8ec637e6ac5ceaa71a0f4420e29
Bryant Kelley <bryantkelley@users.noreply.github.com>
2018-09-16 22:28:14 -0500

    Add user input for additional variables

--- ClientUDP.py
+++ ClientUDP.py
@@ -16,9 +16,13 @@ def main(argv):
         port_number = argv[3]
     except:
         print("Invalid arguments.")
-        #sys.exit()
+        sys.exit()
 
     print(client, server_name, port_number)
+    
+    op_code = input('Opcode: ')
+    operand_one = input('Operand 1: ')
+    operand_two = input('Operand 2: ')
 
 if __name__ == '__main__':
     main(sys.argv)
