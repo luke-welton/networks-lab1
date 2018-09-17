94ea67609d54354267326697f23a561c0e7d582b
Luke Welton <lmoowelton@gmail.com>
2018-09-16 20:30:44 -0500

    stubs for all functions

---
+++ ServerTCP.py
@@ -0,0 +1,13 @@
+def socket(family, type, protocol):
+
+def bind(sockfd, servaddr, addrlen):
+
+def listen(sockfd, backlog):
+
+def accept(sockfd, servaddr, addrlen):
+
+def read():
+
+def write():
+
+def close(sockfd):
