9b65dfea547d0d8f743f358247b7ee86b86d4ebd
jlr0045 <jroach12345@gmail.com>
2018-09-17 23:09:30 -0500

    First Commit with code
    Not much here, most work has been in playing with example code to understand what this is meant to do.

---
+++ ServerUDP.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+
+void displayBuffer(char *Buffer, int length);
+
+int main (int argc, char *argv[])
+{
+
+    //check for command line args with port number
+    if (argc != 2) {
+        fprintf(stderr,"usage: ServerUDP Port# \n");
+        exit(1);
+    }
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//display content of buffer in hexadecimal
+void displayBuffer(char *Buffer, int length){
+    int currentByte, column;
+
+    currentByte = 0;
+    printf("\n>>>>>>>>>>>> Content in hexadecimal <<<<<<<<<<<\n");
+    while (currentByte < length){
+        printf("%3d: ", currentByte);
+        column =0;
+        while ((currentByte < length) && (column < 10)){
+            printf("%2x ",Buffer[currentByte]);
+            column++;
+            currentByte++;
+        }
+        printf("\n");
+    }
+    printf("\n\n");
+}
