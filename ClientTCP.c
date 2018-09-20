e1867d0192c906c7ae166a912c4c3de77eb65d8c
Luke Welton <lmoowelton@gmail.com>
2018-09-20 08:50:13 -0500

    work on tcp client

--- ClientTCP.c
+++ ClientTCP.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/socket.h>
 
 int main (int argc, char *argv[]) {
     if (argc < 2) {
@@ -8,4 +9,12 @@ int main (int argc, char *argv[]) {
     }
 
 
+    int sockFD = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockFD < 0) {
+        printf("An error occurred while creating the client socket. Shutting down.");
+        exit(0);
+    }
+
+
 }
+
